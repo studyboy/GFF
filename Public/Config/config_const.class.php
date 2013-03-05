da59726096470ad4742e620f20ba26a1dbe1f528
gsw <johngao@qq.com>
2013-03-06 00:26:03 +0800

    建立系统配置参数和统一定义系统常量。

---
+++ Public/Config/config_const.class.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * 系统常量统一定义
+ * @version 2013/03/06
+ */
+//系统绝对路径定义
+define('PROTECT_ROOT','');
+define('APP_CONTROLLER_PATH',PROTECT_ROOT.'/Application/Controller/');
+define('APP_MODULE_PATH',PROTECT_ROOT.'/Application/Module/');
+define('APP_VIEW_PATH',PROTECT_ROOT.'/Application/Views/');
+define('APP_PUBLIC_PATH',PROTECT_ROOT.'/Public/');
+define('APP_LIBRARY_PATH',PROTECT_ROOT.'/Library/');
+
+
+
