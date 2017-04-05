4743d387f377f80dcd4e1df0f9916f8555450416
studyboy <johngao@qq.com>
2017-04-05 18:44:49 +0800

    應用類的修改

--- vendor/gff/src/Eros/Foundation/application.php
+++ vendor/gff/src/Eros/Foundation/application.php
@@ -4,5 +4,13 @@ use Eros\Contracts\Foundation\ApplicationInterface;
 use Eros\Container\Container;
 
 class Application extends Container implements ApplicationInterface{
+	/**
+	 * (non-PHPdoc)
+	 * @see Eros\Contracts\Foundation.ApplicationInterface::version()
+	 */
+	public function version(){
+		return '';
+	}
+	
 	
 }
